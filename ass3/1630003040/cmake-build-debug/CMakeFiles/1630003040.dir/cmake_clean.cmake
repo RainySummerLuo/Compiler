76d8565aa70222cd30e67b660e0d4347d7c09586
Zichen Luo <iluo@163.com>
2019-04-01 00:27:17 +0800

    Upload Ass3 files

---
+++ ass3/1630003040/cmake-build-debug/CMakeFiles/1630003040.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1630003040.dir/main.c.obj"
+  "1630003040.pdb"
+  "1630003040.exe"
+  "1630003040.exe.manifest"
+  "lib1630003040.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/1630003040.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
